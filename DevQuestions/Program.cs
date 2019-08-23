a83b352d632118254dfc8fa062928210a38d9fcb
Alejandro Scalise <alejandro.scalise@accessbookings.com>
2019-08-23 11:48:24 +0100

    minor change

--- DevQuestions/Program.cs
+++ DevQuestions/Program.cs
@@ -8,6 +8,7 @@ namespace DevQuestions
     {
         private static void Main(string[] args)
         {
+ 
             #region Question 1 - what's missing
             //
             //
@@ -31,11 +32,11 @@ namespace DevQuestions
             }
             #endregion
 
-            #region Question 3 - give a really short & simple answer as to why
-            //
-            //
-            //
-            #endregion
+            //#region Question 3 - give a really short & simple answer as to why
+            ////
+            ////
+            ////
+            //#endregion
 
             #region Question 4 - improve this code
 
@@ -44,38 +45,38 @@ namespace DevQuestions
 
             PersonRepo personRepo = new PersonRepo();
 
-            List<Person> p = personRepo.GetAll().ToList();
+            List<Person> person = personRepo.GetAll().ToList();
 
-            p = p.Where(somebody => somebody.Age >= 18 & somebody.Age < 99).ToList();
+            person = person.Where(somebody => somebody.Age >= 18 & somebody.Age < 99).ToList();
 
-            p = p.Where(somebody => somebody.Location == "UK").ToList();
+            person = person.Where(somebody => somebody.Location == "UK").ToList();
 
-            p = p.Where(somebody => somebody.FirstName.StartsWith("L")).ToList();
-
-            #endregion
-
-            #region Question 5 - just for a bit of fun, what's this?
-
-            // extra point if you can tell me what it actually is...
-
-            int a = 3, b = 9, c = 24;
-            int theAnswer = (a * a) + (a * c) + (-b * a) + (-b * c);
+            person = person.Where(somebody => somebody.FirstName.StartsWith("L")).ToList();
 
             #endregion
 
-            #region Question 6 - how would you go about getting a value from an Umbraco property?
-            //
-            //
-            //
-            #endregion
+            //#region Question 5 - just for a bit of fun, what's this?
 
-            #region Answers + Question 6 - add the missing code
+            //// extra point if you can tell me what it actually is...
 
-            Answers.ForEach(answer => Console.WriteLine(answer));
+            //int a = 3, b = 9, c = 24;
+            //int theAnswer = (a * a) + (a * c) + (-b * a) + (-b * c);
 
-            Console.ReadLine();
+            //#endregion
 
-            #endregion
+            //#region Question 6 - how would you go about getting a value from an Umbraco property?
+            ////
+            ////
+            ////
+            //#endregion
+
+            //#region Answers + Question 6 - add the missing code
+
+            //Answers.ForEach(answer => Console.WriteLine(answer));
+
+            //Console.ReadLine();
+
+            //#endregion
         }
 
     }
